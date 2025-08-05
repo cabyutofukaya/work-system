0095c2ef36523fc2397be23c334d2ff5d5c1ba07
cabyutofukaya <yuto.fukaya@cab-station.com>
2025-08-05 09:06:02 +0900

    first_commit

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -16,43 +16,19 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
      * @return void
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function update($user, array $input,string $file_name = ''): void
+    public function update($user, array $input): void
     {
 
         Validator::make($input, [
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                // 論理削除済みユーザのメールアドレスを設定可能にする
-                Rule::unique('users')->whereNull('deleted_at')->ignore($user->id),
-            ],
             'tel' => ["nullable", 'regex:/^[0-9][-0-9]+[0-9]$/'],
             'department' => ["nullable", "string"],
-            'name_kana' => [
-                "required",
-                'regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u',
-            ],
-            'hobby' => ["nullable"],
-            'skill' => ["nullable"],
-            'food' => ["nullable"],
-            'trip' => ["nullable"],
-            'free' => ["nullable"],
         ])->validateWithBag('updateProfileInformation');
 
-     
+
 
         $user->forceFill([
-            'name_kana' => $input['name_kana'],
-            'email' => $input['email'],
             'tel' => $input['tel'],
             'department' => $input['department'],
-            'hobby' => $input['hobby'],
-            'skill' => $input['skill'],
-            'food' => $input['food'],
-            'trip' => $input['trip'],
-            'free' => $input['free'],
         ])->save();
     }
 }
